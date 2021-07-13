7fe58f53aee467aa9012a1c93b425de4bd6c346e
sung jun Hwang <ittae.com@gmail.com>
2021-07-13 13:19:03 +0900

    Add notes add page

---
+++ lib/notes/notes_add_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NotesAddPage extends StatelessWidget {
+  static const routeName = '/noteAdd';
+  const NotesAddPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
