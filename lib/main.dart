a2dca0b652b75983d0d25e6b53d9dae52cd7c041
sung jun Hwang <ittae.com@gmail.com>
2021-07-15 13:44:01 +0900

    Add hive logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:haru/common/const_values.dart';
 import 'package:haru/histories/histories_page.dart';
-import 'package:haru/notes/notes_add_page.dart';
+import 'package:haru/models/schedule/schedule.dart';
+import 'package:haru/notes/notes_add_modal.dart';
 import 'package:haru/notes/notes_page.dart';
 import 'package:haru/routes/routes.dart';
 import 'package:haru/settings/settings_page.dart';
 import 'package:haru/timer/timer_page.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
-
-import 'package:logger/logger.dart';
-
 import 'common/color/custom_color.dart';
 import 'home/my_home_page.dart';
+import 'models/note/note.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter(ScheduleAdapter());
+  Hive.registerAdapter(NoteAdapter());
+  await Hive.openBox<Schedule>(scheduleBox);
+  await Hive.openBox<Note>(noteBox);
   runApp(MyApp());
 }
-
-Logger logger = Logger();
 
 class MyApp extends StatelessWidget {
   @override
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
         Routes.timer: (context) => TimerPage(),
         Routes.settings: (context) => SettingsPage(),
         Routes.notes: (context) => NotesPage(),
-        Routes.notesAdd: (context) => NotesAddPage(),
+        Routes.notesAdd: (context) => NotesAddModal(),
       },
     );
   }
