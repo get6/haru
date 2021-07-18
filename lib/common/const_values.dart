c520deab8120fb5a73c567b812c4cf08c1214303
sung jun Hwang <ittae.com@gmail.com>
2021-07-18 22:27:15 +0900

    To globals

--- lib/common/const_values.dart
+++ lib/common/const_values.dart
@@ -1,6 +1,8 @@
-// Hive details
+library globals;
+
 import 'package:logger/logger.dart';
 
+final Logger logger = Logger();
 const String noteBox = 'noteBox';
 const String scheduleBox = 'scheduleBox';
-Logger logger = Logger();
+const double borderRadiusCircular = 30.0;
