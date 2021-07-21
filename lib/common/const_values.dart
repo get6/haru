3285415c928aae0e376211519e4c2a146a0d3928
sung jun Hwang <ittae.com@gmail.com>
2021-07-21 15:09:09 +0900

    Add a function

--- lib/common/const_values.dart
+++ lib/common/const_values.dart
@@ -6,3 +6,12 @@ final Logger logger = Logger();
 const String noteBox = 'noteBox';
 const String scheduleBox = 'scheduleBox';
 const double borderRadiusCircular = 30.0;
+
+String padLeftForTime(int number) {
+  return number.toString().padLeft(2, '0');
+}
+
+DateTime getTime(int hour, int minute) {
+  final today = DateTime.now();
+  return DateTime(today.year, today.month, today.day, hour, minute);
+}
