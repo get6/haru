7224cb94bcaa75e194794cd3e851d04c01a3307c
sung jun Hwang <ittae.com@gmail.com>
2021-07-21 15:10:17 +0900

    Editing not found page

--- lib/histories/histories_page.dart
+++ lib/histories/histories_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:haru/common/const_values.dart';
 
 class HistoriesPage extends StatelessWidget {
   static const routeName = '/histories';
@@ -9,7 +10,36 @@ class HistoriesPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(backgroundColor: Colors.white, elevation: 0),
-      body: Container(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/swimming-pool.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                const SizedBox(height: 30),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 90, vertical: 15),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: const Text(
+                    'No Data Found! 😥',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
